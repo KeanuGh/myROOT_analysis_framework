48726180295c6e5748a4683c179cf5e1b5cc27b4
Keanu Ghorbanian <keanu.ghorbanian@cern.ch>
2020-11-06 00:25:06 +0000

    Implemented a Data class containing all the variables needed for a single dataset. Just need to figure out what to do with the plotting functions now.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Welcome to my framework!
+So far it is able to:
+- Read in many root files and concatenate.
+- Read in a cutfile and apply cuts in groups or independently (todo: writeup how to use cutfile). In the file youcan specify which cuts to apply and how, and specify which variables are going to be plotted.
+- Generate a cutflow for the cuts applied, and print out cutflow histograms, a LaTeX table with cut summaries and a terminal printout of table.
+- Plot root data in 1D Histograms with and without cuts applied, and plot overlaid cuts with corresponding acceptance ratios.
+- Plot 2D histograms of multiple variables with cuts applied.
+- Read in mass slices and normalise all to a specific luminosity.
+- Backs up cutfiles and latex table when changes are made, and saves the last analysis data in pickle files for faster readin next time round.
+- Organises plots by cutfile name.
+
+### In the works:
+- Reading in multiple *separate* datasets in order to compare and perform actual analyses
+- Logging: as soon as I get around to learning how to use the python logging library
+- Unit tests: pytest stopped working for me and I haven't gotten round to fixing it yet. Bugs galore!
--- src/analysis/my_analysis.py
+++ src/analysis/my_analysis.py
@@ -6,6 +6,7 @@ from warnings import warn
 # project imports
 import analysis.config as config
 from analysis.cutflow import Cutflow
+from analysis.dataclass import Data
 from utils.plotting_utils import (
     plot_1d_overlay_and_acceptance_cutgroups,
     plot_2d_cutgroups
@@ -34,9 +35,6 @@ from utils.dataframe_utils import (
 )
 
 
-# TODO: Generate logs
-
-
 class Analysis:
     # ===========================
     # ========= SETUP ===========
@@ -51,7 +49,7 @@ class Analysis:
     backup_cutfiles_dir = config.backup_cutfiles_dir
     latex_table_dir = config.latex_table_dir
 
-    def __init__(self, data: Dict[str, Dict[str]],
+    def __init__(self, data_dict: Dict[str, Dict[str]],
                  cutfile: str,
                  lepton: Optional[str],
                  force_rebuild: bool = False,
@@ -61,29 +59,42 @@ class Analysis:
                  etabins: Optional[Union[tuple, list]] = None,
                  ):
         """
-        TODO __init__ doc string
-        - Currently loops over each substep for each dataframe rather than looping over the entire analysis.
-        - Currently applies the same cuts to ALL dataframes. Instead I could allow each dataframe to read in its own
-          cutfile
-        I may want to change these behaviours later.
+        TODO:
+        | - Currently uses dictionary to hold datasets. Probably be better to use a class
+        | - Currently loops over each substep for each dataframe rather than looping over the entire analysis.
+        | - Currently applies the same cuts to ALL dataframes. Instead I could allow each dataframe to read in its own
+         cutfile
+        | - May want to input your own analysis name to label things rather than just going by the name of the cutfile.
+        | - May want to put all methods that act on only a single dataset into the Data class
+        | - figure out how to access the a particular dataset by doing analysis.dataname?? (like df.column)
+        | - Need to figure out logging
 
-        :param data: Dictionary of dictionaries containing paths to root files and the tree to extract from each.
-        The key to the top-level dictionary is the label assigned to the dataset. This variable will eventually contain
-        the entire dataframe and the path to its backup pickle file under 'df' and 'pkl_path' keys.
-        :param cutfile:
-        :param lepton:
-        :param force_rebuild:
-        :param grouped_cutflow:
-        :param phibins:
-        :param etabins:
+        :param data_dict: Dictionary of dictionaries containing paths to root files and the tree to extract from each.
+        The key to the top-level dictionary is the label assigned to the dataset.
+        :param cutfile: path to cutfile
+        :param force_rebuild: Force rebuild all dataframes.
+        :param global_lumi: all data will be scaled to this luminosity
+        :param phibins: bins for plotting phi
+        :param etabins: bins for plotting eta
         """
-        self.data = data
+        # check input
+        for name, dic in data_dict.items():
+            assert set(dic.keys()) == {'name', 'path', 'TTree', 'slices'}, \
+                f"{data_dict[name]} improperly formatted. Possible missing values."
+            assert isinstance(dic['name'], str), "Dataset name must be a string."
+            assert isinstance(dic['path'], str), "Path to root file must be a string."
+            assert isinstance(dic['TTree'], str), "TTree name must be a string."
+            assert isinstance(dic['slices'], bool), "'slices' variable must be boolean."
+
+        # build Data classes in name:Data dictionary containing initial values
+        self.data = {
+            name: Data(name=dic['name'], datapath=dic['path'], TTree_name=dic['TTree'], is_slices=dic['slices'])
+            for name, dic in data_dict
+        }
 
         # set analysis options
-        if lepton:
-            config.lepton = lepton
-        if global_lumi:
-            config.lumi = global_lumi
+        if lepton: config.lepton = lepton
+        if global_lumi: config.lumi = global_lumi
         self._cutfile = cutfile
         self._not_log = [
             '_phi_',
@@ -91,18 +102,12 @@ class Analysis:
         ]
 
         # variables that require special (default) binning
-        if not etabins:
-            config.etabins = etabins
-        if not phibins:
-            config.phibins = phibins
+        if etabins: config.etabins = etabins
+        if phibins: config.phibins = phibins
         self._special_binning = {
             '_eta_': config.etabins,
             '_phi_': config.phibins,
         }
-
-        # generate dataframe pickle filepaths
-        for name in data:
-            self.data[name]['pkl_path'] = config.pkl_df_filepath.format(name)
 
         # ============================
         # ===== PROCESS CUTFILE ======
@@ -126,11 +131,11 @@ class Analysis:
 
         # check which dataframes need to be rebuilt
         for name in self.data:
-            data[name]['rebuild'] = if_build_dataframe(self._cutfile,
-                                                       self._make_backup,
-                                                       self.backup_cutfiles_dir,
-                                                       data[name]['pkl_path']
-                                                       )
+            self.data[name].rebuild = if_build_dataframe(self._cutfile,
+                                                         self._make_backup,
+                                                         self.backup_cutfiles_dir,
+                                                         self.data[name].pkl_path
+                                                         )
 
         # place plots in outputs/plots/<cutfile name>
         self.plot_dir = self.out_plots_dir + self._cutfile_name.rstrip('.txt') + '/'
@@ -140,53 +145,61 @@ class Analysis:
         # ==== EXTRACT & CLEAN DATA =====
         # ===============================
         for name in self.data:
-            if self.data[name]['rebuild'] or force_rebuild:
+            if self.data[name].rebuild or force_rebuild:
                 print(f"\nBuilding {name} dataframe...")
-                self.data[name]['df'] = build_analysis_dataframe(self.cut_dicts,
-                                                                 self.vars_to_cut,
-                                                                 self.data[name]['path'],
-                                                                 self.data[name]['TTree'],
-                                                                 self.data[name]['pkl_path']
-                                                                 )
+                self.data[name].df = build_analysis_dataframe(self.cut_dicts,
+                                                              self.vars_to_cut,
+                                                              self.data[name]['path'],
+                                                              self.data[name]['TTree'],
+                                                              self.data[name]['pkl_path']
+                                                              )
             else:
                 print(f"Reading data for {name} dataframe from {self.data[name]['pkl_path']}...")
-                self.data[name]['df'] = pd.read_pickle(self.data[name]['pkl_path'])
+                self.data[name].df = pd.read_pickle(self.data[name].pkl_path)
 
         # map weights column
         for name in self.data:
-            if self.data[name]['slices']:
-                self.data[name]['df']['weight'] = gen_weight_column_slices(self.data[name]['df'])
+            if self.data[name].is_slices:
+                self.data[name].df['weight'] = gen_weight_column_slices(self.data[name]['df'])
             else:
-                self.data[name]['df']['weight'] = gen_weight_column(self.data[name]['df'])
+                self.data[name].df['weight'] = gen_weight_column(self.data[name]['df'])
 
         # rescale MeV columns to GeV
         for name in self.data:
-            rescale_to_gev(self.data[name]['df'])
+            rescale_to_gev(self.data[name].df)
 
         # ===============================
         # ======= APPLYING CUTS =========
         # ===============================
         for name in self.data:
-            create_cut_columns(self.data[name]['df'],
-                               cut_dicts=self.cut_dicts,
-                               cut_label=self.cut_label,
-                               printout=True)
+            self.data[name].create_cut_columns(cut_dicts=self.cut_dicts,
+                                               cut_label=self.cut_label,
+                                               printout=True)
+            # create_cut_columns(self.data[name].df,
+            #                    cut_dicts=self.cut_dicts,
+            #                    cut_label=self.cut_label,
+            #                    printout=True)
 
         # ===============================
         # ==== CALCULATING LUMI & XS ====
         # ===============================
         for name in self.data:
-            self.cross_section = get_cross_section(self.tree_df)
-            self.luminosity = get_luminosity(self.tree_df, xs=self.cross_section)
+            self.data[name].gen_cross_section()
+            self.data[name].gen_luminosity()
 
         # ===============================
         # ========== CUTFLOW ============
         # ===============================
-        self.cutflow = Cutflow(df=self.tree_df,
-                               cut_dicts=self.cut_dicts,
-                               cutgroups=self.cutgroups if grouped_cutflow else None,
-                               cut_label=self.cut_label,
-                               sequential=self.options['sequential'])
+        for name in self.data:
+            self.data[name].gen_cutflow(cut_dicts=self.cut_dicts,
+                                        cutgroups=self.cutgroups if grouped_cutflow else None,
+                                        cut_label=self.cut_label,
+                                        sequential=self.options['sequential'])
+            # self.data[name].cutflow = Cutflow(df=self.data[name]['df'],
+            #                                   cut_dicts=self.cut_dicts,
+            #                                   cutgroups=self.cutgroups if grouped_cutflow else None,
+            #                                   cut_label=self.cut_label,
+            #                                   sequential=self.options['sequential'])
 
     # ===============================
     # =========== PLOTS =============
@@ -237,6 +250,7 @@ class Analysis:
             )
 
     def gen_cutflow_hist(self,
+                         data_name: str,
                          event: bool = True,
                          ratio: bool = False,
                          cummulative: bool = False,
@@ -254,6 +268,7 @@ class Analysis:
         :param all_plots: it True, plot all
         :return: None
         """
+
         if all_plots:
             event = ratio = cummulative = a_ratio = True
 
@@ -326,6 +341,9 @@ class Analysis:
     # ===============================
     # ========== PRIVATE ============
     # ===============================
+    def __check_single_datafile(func):
+        def wrapper()
+
     def __getbins(self, var_to_plot) -> Tuple[bool, Optional[tuple]]:
         """
         Returns special bins if variable input requires it. Returns None if not a special variable
@@ -344,15 +362,15 @@ class Analysis:
 
 
 if __name__ == '__main__':
-
-    data_dict = {'truth': {
-        'path': '../../data/mc16d_wmintaunu/*',
-        'TTree': 'truth',
-        'slices': False
+    data = {
+        'truth_inclusive': {
+            'path': '../../data/mc16d_wmintaunu/*',
+            'TTree': 'truth',
+            'slices': False
         }
     }
 
-    my_analysis = Analysis(data=data_dict,
+    my_analysis = Analysis(data_dict=data,
                            cutfile='../../options/cutfile.txt',
                            lepton='tau',
                            force_rebuild=False
