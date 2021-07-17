89ec22c0b107a18d1951381730959d2afcad5cf0
KeanuGh <20911987+KeanuGh@users.noreply.github.com>
2021-07-17 18:24:27 +0000

    Fixed issue where having multiple cuts on same variable would cause errors

--- run/run_analysis.py
+++ run/run_analysis.py
@@ -6,28 +6,28 @@ if __name__ == '__main__':
 
     # dataset inputs
     datasets = {
-        'truth': {
-            'datapath': '../data/*.root',
-            'cutfile': '../options/cutfile_EXAMPLE.txt',
-            'TTree_name': 'truth',
-            'is_slices': False,
-            'lepton': 'muon'
-        },
         # 'truth': {
-        #     'datapath': '../../data/mc16d_wminmunu/*.root',
-        #     'cutfile': '../../options/cutfile_truth.txt',
+        #     'datapath': '../data/*.root',
+        #     'cutfile': '../options/cutfile_EXAMPLE.txt',
         #     'TTree_name': 'truth',
         #     'is_slices': False,
         #     'lepton': 'muon'
-        # }
+        # },
+        'truth': {
+            'datapath': '../data/*.root',
+            'cutfile': '../options/wpt_cuts.txt',
+            'TTree_name': 'truth',
+            'is_slices': False,
+            'lepton': 'muon'
+        }
     }
 
-    my_analysis = Analysis(datasets, analysis_label='wminmunu_test_eg', force_rebuild=True)
+    my_analysis = Analysis(datasets, analysis_label='wpt_cuts_ISR_test', force_rebuild=False)
 
     # pipeline
     # my_analysis.plot_mass_slices(ds_name='truth_slices', xvar='MC_WZ_dilep_m_born', logx=True, to_pkl=True)
-    my_analysis.plot_with_cuts(scaling='xs', to_pkl=True)
-    my_analysis.make_all_cutgroup_2dplots(to_pkl=True)
+    my_analysis.plot_with_cuts(scaling='xs', to_pkl=False)
+    my_analysis.make_all_cutgroup_2dplots(to_pkl=False)
     my_analysis.gen_cutflow_hist(all_plots=True)
     my_analysis.cutflow_printout()
     my_analysis.kinematics_printouts()
