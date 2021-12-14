790ae06e05d298997e470155283761a32dc4cb86
keanu <kghorban@cern.ch>
2021-12-14 13:55:41 +0000

    initial commit for cl dataset builder

--- run/build_dataset.py
+++ run/build_dataset.py
@@ -1,44 +1,55 @@
 import argparse
+import pathlib
 
-parser = argparse.ArgumentParser()
+from src.dataset import lumi_year
+
+parser = argparse.ArgumentParser(description="Build singular dataset",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 # INPUT
-parser.add_argument('--datapath', '-d', help='Path to ROOT file(s)',
-                    optional=False)
-parser.add_argument('--name', '-n', help='Dataset name',
-                    optional=True, default='data')
-parser.add_argument('--TTree', '-T', help='Name of default TTree',
-                    optional=False)
-parser.add_argument('--cutfile', '-c', help='Path to cutfile',
-                    optional=False)
-parser.add_argument('--year', '-y', help='data-taking year',
-                    default='2015+2016', optional=True)
-parser.add_argument('--label', '-l', help='dataset label for plots',
-                    optional=True, default='')
-parser.add_argument('--pkl_dir', help='directory of pickle file',
-                    optional=True)
+parser.add_argument('test', help='test')
+
+parser.add_argument('--datapath', '-d', type=pathlib.Path, required=True,
+                    help='Path to ROOT file(s)')
+parser.add_argument('--name', '-n', default='data',
+                    help='Dataset name')
+parser.add_argument('--TTree', '-T', required=True,
+                    help='Name of default TTree')
+parser.add_argument('--cutfile', '-c', type=pathlib.Path, required=True,
+                    help='Path to cutfile')
+parser.add_argument('--label', '-l', default='',
+                    help='dataset label for plots')
+parser.add_argument('--pkl_dir', type=pathlib.Path,
+                    help='directory of pickle file')
+
+lumi_year_group = parser.add_mutually_exclusive_group()
+lumi_year_group.add_argument('--year', '-y', default='2015+2016', choices=lumi_year.keys(),
+                             help='data-taking year')
+lumi_year_group.add_argument('--luminosity', '--lumi', type=float,
+                             help='data luminosity')
 
 # PATHS
-parser.add_argument('output_root_dir', '-o', help='output directory',
-                    optional=False,)
+parser.add_argument('--output_root_dir', '-o',
+                    help='output directory')
 # parser.add_argument('output')
 
 # OPTIONS
-parser.add_argument('--reco', help='whether dataset will contain reconstructed data (will try and guess if not given)',
-                    optional=True, action='store_true')
-parser.add_argument('--truth', help='whether dataset will contain truth data (will try and guess if not given)',
-                    optional=True, action='store_true')
-parser.add_argument('--to_pkl', help='whether to output to a pickle file',
-                    ptional=True)
-parser.add_argument('--lepton', help='name of lepton in W->lnu if applicable',
-                    optional=True, default='lepton')
-parser.add_argument('--chunksize', help='chunksize for uproot ROOT file import',
-                    optional=True, default=1024, type=int)
-parser.add_argument('--no_validate_missing_events', help='do not check for missing events',
-                    optional=True, action='store_false')
-parser.add_argument('--no_validate_duplicated_events', help='do not check for duplicated events',
-                    optional=True, action='store_false')
-parser.add_argument('--no_validate_sum_of_weights', help='do not check if sumofweights is sum of weight_mc for DSIDs',
-                    optional=True, action='store_false')
+parser.add_argument('--reco', action='store_true',
+                    help='whether dataset will contain reconstructed data (will try and guess if not given)')
+parser.add_argument('--truth', action='store_true',
+                    help='whether dataset will contain truth data (will try and guess if not given)')
+parser.add_argument('--to_pkl', action='store_false',
+                    help='whether to output to a pickle file')
+parser.add_argument('--lepton', default='lepton',
+                    help='name of lepton in W->lnu if applicable')
+parser.add_argument('--chunksize', default=1024, type=int,
+                    help='chunksize for uproot ROOT file import')
+parser.add_argument('--no_validate_missing_events', action='store_false',
+                    help='do not check for missing events')
+parser.add_argument('--no_validate_duplicated_events', action='store_false',
+                    help='do not check for duplicated events')
+parser.add_argument('--no_validate_sum_of_weights', action='store_false',
+                    help='do not check if sumofweights is sum of weight_mc for DSIDs')
 
 args = parser.parse_args()
+print(args.test)
