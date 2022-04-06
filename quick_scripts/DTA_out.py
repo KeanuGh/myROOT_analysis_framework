5572adf8a5456d5def5cd8230009ec8f561eba8d
keanu <kghorban@cern.ch>
2022-04-06 11:40:45 +0100

    importing vector branches seems to work now

--- quick_scripts/DTA_out.py
+++ quick_scripts/DTA_out.py
@@ -1,12 +1,13 @@
+import ROOT
 import matplotlib.pyplot as plt
 import pandas as pd
-import uproot
 
 from src.datasetbuilder import lumi_year
 from src.histogram import Histogram1D
 from utils import plotting_utils
+from utils.variable_names import variable_data
 
-filepath = '/home/keanu/Uni_Stuff_Queen_Mary/DTA/histograms_singletau_H.root'
+filepath = '/mnt/D/data/DTA_outputs/user.kghorban.Sh_2211_Wtaunu_H_maxHTpTV2_CVetoBVeto.MC16a.v1.2022-04-01_histograms.root/*.root'
 treename = 'T_s1thv_NOMINAL'
 wanted_cols = [
     'weight',
@@ -33,15 +34,61 @@ wanted_cols = [
 ]
 bins = (30, 20, 1000)
 
-df = pd.DataFrame(uproot.concatenate(f'{filepath}:{treename}', expressions=wanted_cols, library='np'))
+ROOT.gInterpreter.Declare("""
+float getVecVal(ROOT::VecOps::RVec<float> x, int i = 0);
+
+float getVecVal(ROOT::VecOps::RVec<float> x, int i) {
+    if (x.size() > i)  return x[i];
+    else               return NAN;
+}
+""")
+
+Rdf = ROOT.RDataFrame(treename, filepath)
+Rdf = Rdf.Filter("(passTruth == true) & (passReco == true)")
+
+badcols = set()  # save old column names to avoid extracting them later
+for col_name in list(Rdf.GetColumnNames()):
+    col_type = Rdf.GetColumnType(col_name)
+
+    # unravel vector-type columns
+    if "ROOT::VecOps::RVec" in col_type:
+        # skip non-numeric vector types
+        if col_type == "ROOT::VecOps::RVec<string>":
+            badcols.add(col_name)
+
+        elif 'jet' in str(col_name).lower():
+            # create three new columns for each possible jet
+            for i in range(3):
+                Rdf = Rdf.Define(f"{col_name}{i+1}", f"getVecVal({col_name},{i})")
+            badcols.add(col_name)
+
+        else:
+            Rdf = Rdf.Redefine(col_name, f"getVecVal({col_name},0)")
+
+
+cols_to_extract = [c for c in list(Rdf.GetColumnNames())
+                   if c not in badcols]
+for c in cols_to_extract:
+    if "ROOT::VecOps::RVec" in Rdf.GetColumnType(c):
+        print(f"Column {c} is of type {Rdf.GetColumnType(c)}")
+
+# import needed columns to pandas dataframe
+df = pd.DataFrame(Rdf.AsNumpy(columns=[c for c in list(Rdf.GetColumnNames()) if c not in badcols]))
 df.set_index(['mcChannel', 'eventNumber'], inplace=True)
 df.index.names = ['DSID', 'eventNumber']
 
-df_pass = df[df['passReco'] & df['passTruth']]
-weight = df_pass['weight'] * lumi_year['2015+2016'] / df['mcWeight'].sum()
+# rescale GeV columns
+GeV_columns = [
+    column for column in df.columns
+    if (column in variable_data) and (variable_data[column]['units'] == 'GeV')
+]
+df[GeV_columns] /= 1000
 
-TauPt = df_pass['TauPt'].map(lambda x: x[0]) / 1000
-hTauPt = Histogram1D(TauPt, bins, weight, logbins=True)
+# calc weights
+weight = df['weight'] * lumi_year['2015+2016'] / df['mcWeight'].sum()
+
+# plot
+hTauPt = Histogram1D(df['TauPt'], bins, weight, logbins=True)
 ax = hTauPt.plot()
 plotting_utils.set_axis_options(ax, 'TauPt', bins, lepton='Tau', logx=True, logy=True)
 plt.show()
