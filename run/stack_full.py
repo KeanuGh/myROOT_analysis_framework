994d885ceb31d1d4feb773720e19f360da09ad43
kghorban <kghorban@cern.ch>
2024-01-29 20:12:51 +0100

    stack updates - see how bad the preselection actually is

--- run/stack_full.py
+++ run/stack_full.py
@@ -22,83 +22,105 @@ if __name__ == "__main__":
         "wtaunu_lm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Wtaunu_*_maxHTpTV2*/*.root",
             "hard_cut": "TruthBosonM < 120",
-            "label": r"Sh2211 $W\rightarrow\tau\nu$",
+            "label": r"$W\rightarrow\tau\nu$",
             "merge_into": "wtaunu",
         },
         "wtaunu_hm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Wtaunu_mW_120*/*.root",
             "hard_cut": "TruthBosonM >= 120",
-            "label": r"Sh2211 $W\rightarrow\tau\nu$",
+            "label": r"$W\rightarrow\tau\nu$",
             "merge_into": "wtaunu",
         },
         # backgrounds
         "wmunu_lm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Wmunu_maxHTpTV2*/*.root",
             "hard_cut": "TruthBosonM < 120",
-            "label": r"Sh2211 $W\rightarrow\mu\nu$",
+            "label": r"$W\rightarrow\mu\nu$",
             "merge_into": "wmunu",
         },
         "wmunu_hm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Wmunu_mW_120*/*.root",
             "hard_cut": "TruthBosonM >= 120",
-            "label": r"Sh2211 $W\rightarrow\mu\nu$",
+            "label": r"$W\rightarrow\mu\nu$",
             "merge_into": "wmunu",
         },
         "wenu_lm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Wenu_maxHTpTV2*/*.root",
             "hard_cut": "TruthBosonM < 120",
-            "label": r"Sh2211 $W\rightarrow e\nu$",
+            "label": r"$W\rightarrow e\nu$",
             "merge_into": "wenu",
         },
         "wenu_hm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Wenu_mW_120*/*.root",
             "hard_cut": "TruthBosonM >= 120",
-            "label": r"Sh2211 $W\rightarrow e\nu$",
+            "label": r"$W\rightarrow e\nu$",
             "merge_into": "wenu",
         },
         "ztautau_lm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Ztautau_*_maxHTpTV2*/*.root",
             "hard_cut": "TruthBosonM < 120",
-            "label": r"Sh2211 $Z\rightarrow ll$",
+            "label": r"$Z\rightarrow ll$",
             "merge_into": "zll",
         },
         "ztautau_hm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Ztautau_mZ_120*/*.root",
             "hard_cut": "TruthBosonM >= 120",
-            "label": r"Sh2211 $Z\rightarrow ll$",
+            "label": r"$Z\rightarrow ll$",
             "merge_into": "zll",
         },
         "zmumu_lm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Zmumu_maxHTpTV2*/*.root",
             "hard_cut": "TruthBosonM < 120",
-            "label": r"Sh2211 $Z\rightarrow ll$",
+            "label": r"$Z\rightarrow ll$",
             "merge_into": "zll",
         },
         "zmumu_hm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Zmumu_mZ_120*/*.root",
             "hard_cut": "TruthBosonM >= 120",
-            "label": r"Sh2211 $Z\rightarrow ll$",
+            "label": r"$Z\rightarrow ll$",
             "merge_into": "zll",
         },
         "zee_lm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Zee_maxHTpTV2*/*.root",
             "hard_cut": "TruthBosonM < 120",
-            "label": r"Sh2211 $Z\rightarrow ll$",
+            "label": r"$Z\rightarrow ll$",
             "merge_into": "zll",
         },
         "zee_hm": {
             "data_path": DTA_PATH / "user.kghorban.Sh_2211_Zee_mZ_120*/*.root",
             "hard_cut": "TruthBosonM >= 120",
-            "label": r"Sh2211 $Z\rightarrow ll$",
+            "label": r"$Z\rightarrow ll$",
             "merge_into": "zll",
         },
         "ttbar": {
             "data_path": DTA_PATH / "user.kghorban.PP8_ttbar_hdamp258p75*/*.root",
-            "label": r"PP8 $t\bar{t}$",
+            "label": r"$t\bar{t}$",
         },
     }
 
     cuts: dict[str, list[Cut]] = {
+        "tightTau" : [
+            Cut(
+                r"\mathrm{pass trigger}",
+                r"passTrigger",
+            ),
+            Cut(
+                r"\mathrm{tight tau}",
+                r"TauTightWP",
+            ),
+            Cut(
+                r"$p_T^\tau > 170$",
+                r"TauPt > 170",
+            ),
+            Cut(
+                r"$E_T^{\mathrm{miss}} > 100",
+                r"MET_met > 100",
+            ),
+            Cut(
+                r"$m_T^W > 150$",
+                r"MTW > 150",
+            ),
+        ],
         "medTau": [
             Cut(
                 r"\mathrm{pass trigger}",
@@ -142,7 +164,25 @@ if __name__ == "__main__":
                 r"$m_T^W > 150$",
                 r"MTW > 150",
             ),
-        ]
+        ],
+        "passPreselection" : [
+            Cut(
+                r"Pass preselection",
+                r"passReco",
+            ),
+            Cut(
+                r"$p_T^\tau > 170$",
+                r"TauPt > 170",
+            ),
+            Cut(
+                r"$E_T^{\mathrm{miss}} > 100",
+                r"MET_met > 100",
+            ),
+            Cut(
+                r"$m_T^W > 150$",
+                r"MTW > 150",
+            ),
+        ],
     }
 
     wanted_branches = {
@@ -154,19 +194,19 @@ if __name__ == "__main__":
         "MTW",
     }
 
-    datasets_merged = [
-        "data",
+    MC = [
         "wtaunu",
         "wmunu",
         "wenu",
         "zll",
         "ttbar",
     ]
+    datasets_merged = ["data"] + MC
     analysis = Analysis(
         datasets,
         data_dir=DATA_OUT_DIR,
         year="2017",
-        # regen_histograms=True,
+        regen_histograms=True,
         ttree="T_s1thv_NOMINAL",
         cuts=cuts,
         analysis_label="stack_full",
@@ -175,12 +215,12 @@ if __name__ == "__main__":
         log_out="both",
         extract_vars=wanted_branches,
         binnings={
-            "MTW": np.geomspace(150, 2000, 20),
-            "TauPt": np.geomspace(170, 2000, 20),
+            "MTW": np.geomspace(150, 1000, 20),
+            "TauPt": np.geomspace(170, 1000, 20),
             "TauEta": np.linspace(-2.47, 2.47, 20),
             "EleEta": np.linspace(-2.47, 2.47, 20),
             "MuonEta": np.linspace(-2.5, 2.5, 20),
-            "MET_met": np.geomspace(85, 2000, 20),
+            "MET_met": np.geomspace(100, 1000, 20),
         },
     )
     analysis.cutflow_printout(datasets=datasets_merged, latex=True)
@@ -189,18 +229,18 @@ if __name__ == "__main__":
 
     # set colours for merged datasets only
     c_iter = iter(plt.rcParams["axes.prop_cycle"].by_key()["color"])
-    for ds in datasets_merged:
+    for ds in MC:
         c = next(c_iter)
         analysis[ds].colour = c
-        analysis.logger.info(f"Colour for {ds}: {c}")
 
     # HISTORGRAMS
     # ========================================================================
     # argument dicts
     default_args = {
-        "datasets": datasets_merged,
+        "datasets": MC,
         "title": f"mc16d | {analysis.global_lumi/1000:.3g}" + r"fb$^{-1}$",
         "cut": True,
+        "yerr": True,
     }
 
     # mass-like variables
@@ -220,6 +260,7 @@ if __name__ == "__main__":
         "TauPhi",
     ]:
         analysis.stack_plot(var=var, **default_args, data=True)
+        analysis.stack_plot(var=var, **default_args, logy=False, data=True, suffix="liny")
         analysis.plot_hist(var=var, **default_args, ratio_plot=False)
 
     analysis.histogram_printout()
