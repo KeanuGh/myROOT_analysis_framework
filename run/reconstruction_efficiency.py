0a43edf636477c0e22c4bc5bcac854e4fa42bb07
keanu <kghorban@cern.ch>
2024-11-06 20:56:12 +0000

    add trigger efficiencies

--- run/reconstruction_efficiency.py
+++ run/reconstruction_efficiency.py
@@ -29,10 +29,21 @@ datasets: Dict[str, Dict] = {
 # ========================================================================
 pass_presel = Cut(
     r"Pass preselection",
-    r"(passReco == 1) && (TauBaselineWP == 1) && (abs(TauCharge) == 1)"
+    r"(passReco == 1) && (TauBaselineWP == 1) && (abs(TauCharge) == 1) && passMetTrigger"
     # r"&& ((MatchedTruthParticle_isTau + MatchedTruthParticle_isElectron + MatchedTruthParticle_isMuon + MatchedTruthParticle_isPhoton) <= 1)",
 )
-pass_truth = Cut(r"Pass Truth", r"(passTruth == 1)")
+pass_trigger = Cut(
+    r"Pass trigger",
+    r"passTrigger",
+)
+pass_met_trigger = Cut(
+    r"Pass met trigger",
+    r"passMetTrigger",
+)
+pass_truth = Cut(
+    r"Pass Truth",
+    r"(passTruth == 1)",
+)
 truth_tau = Cut(
     r"Truth Hadronic Tau",
     r"TruthTau_isHadronic",
@@ -84,7 +95,7 @@ pass_SR_truth = Cut(
 )
 pass_matched_truth_tau = Cut(
     r"Matched Truth Tau",
-    r"MatchedTruthParticle_isHadronicTau == true",
+    r"(MatchedTruthParticle_isHadronicTau == true) && (MatchedTruthParticlePt == TruthTauPt)",
 )
 selections: dict[str, list[Cut]] = {
     "truth_tau": [
@@ -102,6 +113,34 @@ selections: dict[str, list[Cut]] = {
         pass_SR_truth,
         truth_tau,
         truth_tau_3prong,
+    ],
+    "1prong_truth_tau_trigger": [
+        pass_truth,
+        pass_SR_truth,
+        truth_tau,
+        truth_tau_1prong,
+        pass_trigger,
+    ],
+    "3prong_truth_tau_trigger": [
+        pass_truth,
+        pass_SR_truth,
+        truth_tau,
+        truth_tau_3prong,
+        pass_trigger,
+    ],
+    "1prong_truth_tau_mettrigger": [
+        pass_truth,
+        pass_SR_truth,
+        truth_tau,
+        truth_tau_1prong,
+        pass_met_trigger,
+    ],
+    "3prong_truth_tau_mettrigger": [
+        pass_truth,
+        pass_SR_truth,
+        truth_tau,
+        truth_tau_3prong,
+        pass_met_trigger,
     ],
     "vl_reco_tau": [
         pass_presel,
@@ -187,26 +226,6 @@ selections: dict[str, list[Cut]] = {
 
 # VARIABLES
 # ========================================================================
-wanted_variables = {
-    "TauEta",
-    "TauPhi",
-    "TauPt",
-    "TauRNNJetScore",
-    "TauBDTEleScore",
-    "TruthTauPt",
-    "TruthTauEta",
-    "TruthTauPhi",
-    "TauNCoreTracks",
-    "TauPt_res",
-    "TauPt_diff",
-    "MatchedTruthParticlePt",
-    "MatchedTruthParticle_isTau",
-    "MatchedTruthParticle_isElectron",
-    "MatchedTruthParticle_isMuon",
-    "MatchedTruthParticle_isPhoton",
-    "MatchedTruthParticle_isJet",
-    "nJets",
-}
 measurement_vars_mass = [
     "TauPt",
     "TruthTauPt",
@@ -223,6 +242,8 @@ measurement_vars_unitless = [
     "TruthTauPhi",
     "VisTruthTauEta",
     "VisTruthTauPhi",
+    "TruthNeutrinoPt",
+    "TruthNeutrinoEta",
     "nJets",
     "TruthTau_nChargedTracks",
     "TruthTau_nNeutralTracks",
@@ -235,7 +256,6 @@ measurement_vars_unitless = [
     "TruthTauPt_div_MET",
 ]
 measurement_vars = measurement_vars_unitless + measurement_vars_mass
-wanted_variables |= set(measurement_vars)
 NOMINAL_NAME = "T_s1thv_NOMINAL"
 
 
@@ -244,8 +264,8 @@ def run_analysis() -> Analysis:
     return Analysis(
         datasets,
         year=2017,
-        rerun=True,
-        regen_histograms=True,
+        # rerun=True,
+        # regen_histograms=True,
         do_systematics=DO_SYS,
         # regen_metadata=True,
         ttree=NOMINAL_NAME,
@@ -253,33 +273,32 @@ def run_analysis() -> Analysis:
         analysis_label="reconstruction_efficiency",
         log_level=10,
         log_out="both",
-        extract_vars=wanted_variables,
+        extract_vars=measurement_vars,
         import_missing_columns_as_nan=True,
         binnings={
             "": {
-                "MTW": np.geomspace(150, 1000, 21),
-                "TruthMTW": np.geomspace(150, 1000, 21),
-                "TauPt": np.geomspace(170, 1000, 21),
-                "TruthTauPt": np.geomspace(170, 1000, 21),
-                "VisTruthTauPt": np.geomspace(170, 1000, 21),
+                "MTW": np.geomspace(150, 1000, 11),
+                "TruthMTW": np.geomspace(150, 1000, 11),
+                "TauPt": np.geomspace(170, 1000, 11),
+                "TruthTauPt": np.geomspace(170, 1000, 11),
+                "VisTruthTauPt": np.geomspace(170, 1000, 11),
+                "TruthNeutrinoPt": np.geomspace(170, 1000, 11),
                 "TruthTau_nChargedTracks": np.array([0.0, 1.0, 2.0, 3.0, 4.0]),
-                "TruthTau_nNeutralTracks": np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 7.0]),
-                "TauEta": np.linspace(-2.5, 2.5, 21),
-                "TruthTauEta": np.linspace(-2.5, 2.5, 21),
-                "VisTruthTauEta": np.linspace(-2.5, 2.5, 21),
-                "EleEta": np.linspace(-2.5, 2.5, 21),
-                "MuonEta": np.linspace(-2.5, 2.5, 21),
-                "MET_met": np.geomspace(150, 1000, 21),
-                "DeltaPhi_tau_met": np.linspace(0, 3.5, 21),
-                "TruthDeltaPhi_tau_met": np.linspace(0, 3.5, 21),
-                "TauPt_div_MET": np.linspace(0, 3, 61),
-                "TruthTauPt_div_MET": np.linspace(0, 3, 61),
+                "TruthTau_nNeutralTracks": np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0]),
+                "TauEta": np.linspace(-2.5, 2.5, 11),
+                "TruthTauEta": np.linspace(-2.5, 2.5, 11),
+                "VisTruthTauEta": np.linspace(-2.5, 2.5, 11),
+                "TruthNeutrinoEta": np.linspace(-2.5, 2.5, 11),
+                "MET_met": np.geomspace(150, 1000, 11),
+                "DeltaPhi_tau_met": np.linspace(0, 3.5, 11),
+                "TruthDeltaPhi_tau_met": np.linspace(0, 3.5, 11),
+                "TauPt_div_MET": np.linspace(0, 3, 21),
+                "TruthTauPt_div_MET": np.linspace(0, 3, 21),
                 "TauRNNJetScore": np.linspace(0, 1, 51),
                 "TauBDTEleScore": np.linspace(0, 1, 51),
                 "TauNCoreTracks": np.linspace(0, 4, 5),
-                "TauPt_res": np.linspace(-1, 1, 51),
-                "TauPt_diff": np.linspace(-300, 300, 51),
-                "badJet": (2, 0, 2),
+                "TauPt_res": np.linspace(-1, 1, 31),
+                "TauPt_diff": np.linspace(-300, 300, 31),
             },
             ".*_CR_.*ID": {
                 "MET_met": np.geomspace(1, 100, 51),
@@ -300,7 +319,7 @@ if __name__ == "__main__":
         dataset.histogram_printout(to_file="txt", to_dir=analysis.paths.latex_dir)
 
     # calculate efficiencies
-    efficiencies = {
+    recon_efficiencies = {
         wp: {
             nprong: {
                 var: (
@@ -313,20 +332,158 @@ if __name__ == "__main__":
         }
         for wp in ("vl", "loose", "medium", "tight")
     }
+    trigger_efficiencies = {
+        nprong: {
+            var: (
+                analysis.get_hist(var, "wtaunu", NOMINAL_NAME, f"{nprong}truth_tau_trigger")
+                / analysis.get_hist(var, "wtaunu", NOMINAL_NAME, f"{nprong}truth_tau")
+            )
+            for var in measurement_vars
+        }
+        for nprong in ("1prong_", "3prong_")
+    }
+    met_trigger_efficiencies = {
+        nprong: {
+            var: (
+                analysis.get_hist(var, "wtaunu", NOMINAL_NAME, f"{nprong}truth_tau_mettrigger")
+                / analysis.get_hist(var, "wtaunu", NOMINAL_NAME, f"{nprong}truth_tau")
+            )
+            for var in measurement_vars
+        }
+        for nprong in ("1prong_", "3prong_")
+    }
 
     default_args = {
         "dataset": "wtaunu",
         "title": f"mc16d | {analysis.global_lumi / 1000:.3g}" + r"fb$^{-1}$",
         "do_stat": False,
         "do_sys": False,
-        "ylabel": r"Reconstruction Efficiency \epsilon",
         "selection": "",
         "ratio_plot": False,
         "stats_box": False,
+        "ylabel": r"Trigger Efficiency $\epsilon_\mathrm{trigger}$",
     }
+
+    # TRIGGER EFFICIENCY
+    # ========================================================================
+    # reco
+    analysis.paths.plot_dir = base_plotting_dir / "trigger_efficiency"
+    for s, effs in (("trigger", trigger_efficiencies), ("met_trigger", met_trigger_efficiencies)):
+        analysis.plot(
+            val=[
+                effs["1prong_"]["TauPt"],
+                effs["3prong_"]["TauPt"],
+            ],
+            label=["1-prong", "3-prong"],
+            **default_args,
+            logx=True,
+            xlabel=variable_data["TauPt"]["name"] + " [GeV]",
+            filename=f"TauPt_{s}_efficiency_1prong_3prong.png",
+        )
+        analysis.plot(
+            val=[
+                effs["1prong_"]["TauEta"],
+                effs["3prong_"]["TauEta"],
+            ],
+            label=["1-prong", "3-prong"],
+            **default_args,
+            logx=False,
+            xlabel=variable_data["TauEta"]["name"],
+            filename=f"TauEta_{s}_efficiency_1prong_3prong.png",
+        )
+        # truth
+        analysis.plot(
+            val=[
+                effs["1prong_"]["TruthTauPt"],
+                effs["3prong_"]["TruthTauPt"],
+            ],
+            label=["1-prong", "3-prong"],
+            **default_args,
+            logx=True,
+            xlabel=variable_data["TruthTauPt"]["name"] + " [GeV]",
+            filename=f"TruthTauPt_{s}_efficiency_1prong_3prong.png",
+        )
+        analysis.plot(
+            val=[
+                effs["1prong_"]["VisTruthTauPt"],
+                effs["3prong_"]["VisTruthTauPt"],
+            ],
+            label=["1-prong", "3-prong"],
+            **default_args,
+            logx=True,
+            xlabel=variable_data["VisTruthTauPt"]["name"] + " [GeV]",
+            filename=f"VisTruthTauPt_{s}_efficiency_1prong_3prong.png",
+        )
+        analysis.plot(
+            val=[
+                effs["1prong_"]["TruthNeutrinoPt"],
+                effs["3prong_"]["TruthNeutrinoPt"],
+            ],
+            label=["1-prong", "3-prong"],
+            **default_args,
+            logx=True,
+            xlabel=variable_data["TruthNeutrinoPt"]["name"] + " [GeV]",
+            filename=f"TruthNeutrinoPt_{s}_efficiency_1prong_3prong.png",
+        )
+        analysis.plot(
+            val=[
+                effs["1prong_"]["TruthTauEta"],
+                effs["3prong_"]["TruthTauEta"],
+            ],
+            label=["1-prong", "3-prong"],
+            **default_args,
+            logx=False,
+            xlabel=variable_data["TruthTauEta"]["name"],
+            filename=f"TruthTauEta_{s}_efficiency_1prong_3prong.png",
+        )
+        analysis.plot(
+            val=[
+                effs["1prong_"]["VisTruthTauEta"],
+                effs["3prong_"]["VisTruthTauEta"],
+            ],
+            label=["1-prong", "3-prong"],
+            **default_args,
+            logx=False,
+            xlabel=variable_data["VisTruthTauEta"]["name"],
+            filename=f"VisTruthTauEta_{s}_efficiency_1prong_3prong.png",
+        )
+        analysis.plot(
+            val=[
+                effs["1prong_"]["TruthNeutrinoEta"],
+                effs["3prong_"]["TruthNeutrinoEta"],
+            ],
+            label=["1-prong", "3-prong"],
+            **default_args,
+            logx=False,
+            xlabel=variable_data["TruthNeutrinoEta"]["name"],
+            filename=f"TruthNeutrinoEta_{s}_efficiency_1prong_3prong.png",
+        )
+        analysis.plot(
+            val=[
+                effs["1prong_"]["TruthTau_nNeutralTracks"],
+                effs["3prong_"]["TruthTau_nNeutralTracks"],
+            ],
+            label=["1-prong", "3-prong"],
+            **default_args,
+            logx=False,
+            xlabel=variable_data["TruthTau_nNeutralTracks"]["name"],
+            filename=f"TruthTau_nNeutralTracks_{s}_efficiency_1prong_3prong.png",
+        )
+        analysis.plot(
+            val=[
+                effs["1prong_"]["TruthTau_nChargedTracks"],
+                effs["3prong_"]["TruthTau_nChargedTracks"],
+            ],
+            label=["1-prong", "3-prong"],
+            **default_args,
+            logx=False,
+            xlabel=variable_data["TruthTau_nChargedTracks"]["name"],
+            filename=f"TruthTau_nChargedTracks_{s}_efficiency_1prong_3prong.png",
+        )
 
     # START OF PONG LOOP
     # ========================================================================
+    default_args["ylabel"] = r"Reconstruction Efficiency $\epsilon$"
     wps = ("vl", "loose", "medium", "tight")
     for nprong in ("", "1prong_", "3prong_"):
         analysis.paths.plot_dir = base_plotting_dir / nprong
@@ -335,7 +492,7 @@ if __name__ == "__main__":
         # ========================================================================
         for v in measurement_vars_mass:
             analysis.plot(
-                val=[efficiencies[wp][nprong][v] for wp in wps],
+                val=[recon_efficiencies[wp][nprong][v] for wp in wps],
                 label=["Very Loose", "Loose", "Medium", "Tight"],
                 **default_args,
                 logx=True,
@@ -344,7 +501,7 @@ if __name__ == "__main__":
             )
         for v in measurement_vars_unitless:
             analysis.plot(
-                val=[efficiencies[wp][nprong][v] for wp in wps],
+                val=[recon_efficiencies[wp][nprong][v] for wp in wps],
                 label=["Very Loose", "Loose", "Medium", "Tight"],
                 **default_args,
                 xlabel=variable_data[v]["name"],
@@ -361,7 +518,7 @@ if __name__ == "__main__":
         # ========================================================================
         for v in measurement_vars_mass:
             analysis.plot(
-                val=[efficiencies[wp]["1prong_"][v], efficiencies[wp]["3prong_"][v]],
+                val=[recon_efficiencies[wp]["1prong_"][v], recon_efficiencies[wp]["3prong_"][v]],
                 label=["1-prong", "3-prong"],
                 **default_args,
                 logx=True,
@@ -370,7 +527,7 @@ if __name__ == "__main__":
             )
         for v in measurement_vars_unitless:
             analysis.plot(
-                val=[efficiencies[wp]["1prong_"][v], efficiencies[wp]["3prong_"][v]],
+                val=[recon_efficiencies[wp]["1prong_"][v], recon_efficiencies[wp]["3prong_"][v]],
                 label=["1-prong", "3-prong"],
                 **default_args,
                 xlabel=variable_data[v]["name"],
