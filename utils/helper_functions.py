f652b66fbc69efaaa7f2cf8d96cd6b4df1639399
keanu <kghorban@cern.ch>
2024-11-25 16:11:46 +0000

    delete some old unused functions

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,39 +1,6 @@
-import os
 from glob import glob
 from pathlib import Path
 from warnings import warn
-
-
-def is_dir_empty(dirpath: str) -> bool:
-    """checks if input directory is empty"""
-    return len(os.listdir(dirpath)) == 0
-
-
-def get_filename(filepath: str | Path, suffix: bool = False) -> str:
-    """gets the name of file contained in path"""
-    if suffix:
-        return Path(filepath).name
-    else:
-        return Path(filepath).stem
-
-
-def get_file_parent(filepath: str) -> Path:
-    """Get full path of directory containing file"""
-    return Path(filepath).parent
-
-
-def file_exists(filepath: str | Path) -> bool:
-    """Does the file exist?"""
-    if isinstance(filepath, Path):
-        return bool(filepath.parent.rglob("*"))
-    return bool(glob(filepath))
-
-
-def n_files(filepath: str | Path) -> int:
-    """how many files?"""
-    if isinstance(filepath, Path):
-        return len(list(filepath.parent.rglob("*.root")))
-    return len(glob(filepath))
 
 
 def multi_glob(paths: Path | list[Path] | str | list[str]) -> list[str]:
@@ -52,7 +19,6 @@ def multi_glob(paths: Path | list[Path] | str | list[str]) -> list[str]:
     return all_files
 
 
-# didn't know where else to put this
 def smart_join(*s: str | None, sep: str = "_") -> str:
     """Concatenated string with separator, ignoring any blank strings or None"""
     return sep.join(filter(None, s))
