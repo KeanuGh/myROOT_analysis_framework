877a5ff93d09b5e8d19eae97b42cd135e17a4ae4
keanu <kghorban@cern.ch>
2026-06-08 23:02:17 +0100

    ROOT typing

---
+++ typings/ROOT/__init__.pyi
@@ -0,0 +1,49 @@
+from typing import Any
+
+
+Detail: Any
+EnableImplicitMT: Any
+Experimental: Any
+PyConfig: Any
+RDF: Any
+RDataFrame: Any
+RResultsPtr: Any
+RooUnfoldBayes: Any
+RooUnfoldBinByBin: Any
+RooUnfoldResponse: Any
+RooUnfoldSvd: Any
+SetAtlasStyle: Any
+TCanvas: Any
+TChain: Any
+TF1: Any
+TFile: Any
+TGraphAsymmErrors: Any
+TH1: Any
+TH1C: Any
+TH1D: Any
+TH1F: Any
+TH1I: Any
+TH1S: Any
+TH2: Any
+TH2C: Any
+TH2D: Any
+TH2F: Any
+TH2I: Any
+TH2S: Any
+TH3: Any
+TH3C: Any
+TH3D: Any
+TH3F: Any
+TH3I: Any
+TH3S: Any
+TProfile: Any
+TProfile1DModel: Any
+addressof: Any
+gDirectory: Any
+gInterpreter: Any
+gROOT: Any
+gStyle: Any
+gSystem: Any
+
+
+def __getattr__(name: str) -> Any: ...
